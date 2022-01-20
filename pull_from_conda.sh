168651df5f6220af50ceab382b153f8c1f89ecb0
Jose H. Garcia <86189189+josehgarciaa@users.noreply.github.com>
2022-01-20 15:31:01 +0100

    Created conda preparation script
    This scrip at this point install conda and the latest available python version

---
+++ pull_from_conda.sh
@@ -0,0 +1,21 @@
+PYTHON_ROOT="/usr/local/lib/"
+LATEST_PYTHON_PATH=`ls -d ${PYTHON_ROOT}python* |tail -n1`;
+LATESTS_PYTHON="${LATEST_PYTHON_PATH/${PYTHON_ROOT}/""}"    
+python_version="${latest_python/n/"n="}"    
+
+CONDA_ROOT="/usr/local/lib/"
+
+if [ ! -f $CONDA_ROOT"/conda"  ]; then
+echo "Installing packages for the activity"
+export PYTHONPATH=""
+MINICONDA_INSTALLER_SCRIPT=Miniconda3-latest-Linux-x86_64.sh
+MINICONDA_PREFIX=/usr/local
+wget https://repo.continuum.io/miniconda/$MINICONDA_INSTALLER_SCRIPT&>log
+chmod +x $MINICONDA_INSTALLER_SCRIPT
+./$MINICONDA_INSTALLER_SCRIPT -b -f -p $MINICONDA_PREFIX&>log
+conda install --channel defaults conda python_version --yes&>log
+conda update --channel defaults --all --yes&>log
+#conda install -c conda-forge kwant &>log
+fi
+export PYTHONPATH=$PYTHONPATH:${LATEST_PYTHON_PATH}site-packages
+echo "All requested packages already installed.
