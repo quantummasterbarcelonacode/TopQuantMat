7a95c5ea1b0a8ba2d63f895ed21e7615a4eb950b
Jose H. Garcia <86189189+josehgarciaa@users.noreply.github.com>
2022-01-20 15:50:52 +0100

    Added a symbolic link to the latest python dir

--- pull_from_conda.sh
+++ pull_from_conda.sh
@@ -3,10 +3,6 @@ LATEST_PYTHON_PATH=`ls -d ${PYTHON_ROOT}python* |tail -n1`;
 LATEST_PYTHON="${LATEST_PYTHON_PATH/${PYTHON_ROOT}/""}"    
 python_version="${LATEST_PYTHON/n/"n="}"    
 
-echo $LATEST_PYTHON_PATH
-echo $LATEST_PYTHON
-echo $python_version
-
 CONDA_ROOT="/usr/local/bin/"
 if [ ! -f $CONDA_ROOT"/conda"  ]; then
 echo "Installing packages for the activity"
@@ -21,5 +17,10 @@ conda update --channel defaults --all --yes#&>log
 fi
 export PYTHONPATH=$PYTHONPATH:"${LATEST_PYTHON_PATH}site-packages"
 conda install -c conda-forge kwant 
+conda install -c conda-forge qsymm
+conda install -c conda-forge sympy
 
+#Add a path to the content
+ln -s LATEST_PYTHON_PATH/site-packages /content/conda_dir
+echo ${LATEST_PYTHON_PATH}/site-packages
 echo "All requested packages already installed."
